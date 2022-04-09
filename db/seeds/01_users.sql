e29be55551f55c1200c510ae54d9dae369db5530
double-slide <93881358+double-slide@users.noreply.github.com>
2022-04-09 23:38:30 +0000

    Add user seed file

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, profile_image_url)
+VALUES ('Spidey', 'https://i.pinimg.com/474x/c2/4b/93/c24b93d9ca0b94291368cacded0d73f1.jpg');
+
+INSERT INTO users (name, profile_image_url)
+VALUES ('Monica', 'https://static.wikia.nocookie.net/friends/images/e/ee/Monica_Season_1.jpg/revision/latest/smart/width/250/height/250?cb=20200604100810');
+
+INSERT INTO users (name, profile_image_url)
+VALUES ('Forest', 'https://c-cl.cdn.smule.com/smule-gg-s-sf-bck1/arr/3e/1c/3c98860e-7e92-44de-84cb-9c8e1f80814a.jpg');
