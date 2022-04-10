1e3db4c8eb6e8cb6f9d6bfbcfa6063eade2c3188
double-slide <93881358+double-slide@users.noreply.github.com>
2022-04-10 00:48:06 +0000

    Add seeds for map pins

---
+++ db/seeds/04_pins.sql
@@ -0,0 +1,59 @@
+
+-- CREATED BY SPIDEY
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 1, 40.717149, -73.798003, 'Doc Oc', 'Spotted Doc Oc destroying cars', 'https://64.media.tumblr.com/2dbfbb3172db9f2146a69773a0574d4e/52d9f7b7d0937373-4f/s128x128u_c1/f0b1e4fea0c71430a72bbe231228c73b305266ed.jpg');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 1, 50.015083, -110.693254, 'Med Hat Rat', 'Spotted the Rat spray painting the teepee', 'https://lh5.googleusercontent.com/p/AF1QipPJO1k4x1RWW3iB0AC7Ji4QLNvZE-1tPSG-S82F=w408-h306-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 2, 40.695034,-73.906084, 'Queens Coffee', 'Best coffee in queens', 'https://lh5.googleusercontent.com/p/AF1QipP5plW4M9waD9HS74aWQ3QcTsu1DDKJ0gmHTjuX=w408-h510-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 2, 40.699249,-73.9471217, 'Bond Collective', 'Boujee coffee shop in Brooklyn', 'https://lh5.googleusercontent.com/p/AF1QipNkBxry6YuArAFRziaSBWcwRQ-cnyxf8U3OEXVZ=w408-h272-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 4, 30.2973599,-87.617019, 'Orange Beach', 'More of a city but still on the water', 'https://lh6.googleusercontent.com/proxy/Zgicbu0BRgqIo3gxD8aOsU7S09UHns32_Dic-54KzxYN4A8mlpm8GXBkal1Lo7ZyrjpUlju6rHQdOVgtdvCBb74kC7A5SbnkrhgThGxai2NAjoheWqWs8pkkdcklafTflIeK9bH25yMy6vQJN5tz_UVixxWySXM=w408-h270-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (1, 4, 30.2921629,-87.5881799, 'Alabama Point East', 'The best beach ever', 'https://lh5.googleusercontent.com/p/AF1QipMZ4bY9oYa_GmnpgFVRr4AZcS7nk_NDka1MLD7Q=w408-h306-k-no');
+
+
+-- CREATED BY MONICA
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (2, 2, 40.7639452, -73.9849077, 'Ground Central Coffee Company', 'GC is a one-of-a-kind independent coffeeshop serving a distinct La Colombe blend, and a variety of delicacies from local food providers.', 'https://lh5.googleusercontent.com/p/AF1QipNlHtTHIO40nqZv-C28FU8e9mjCwlJbg_U4Pk7K=w408-h272-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (2, 3, 40.721264, -74.0188686, 'Banter NYC', 'A local cafe in Greenwich Village Cozy with a patio featuring elevated, health-conscious fare, plus espresso & select cocktails.', 'https://3.bp.blogspot.com/-VIN_ORaAnUs/WJ9DICYckHI/AAAAAAAAbn0/W3KG0DW77mU99Gm_NjBnBobvV4RoT6lmQCEw/s1600/Banter-NYC-Brunch-Sullivan-6.jpg');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title)
+VALUES (2, 1, 40.39.0515761, -77.1474078, 'The White House');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (2, 1, 37.8269775,-122.4251442, 'Alcatraz Island', 'Notorious prison & once home to Al Capone, now a museum with audio tour, accessible by ferry.', 'https://lh5.googleusercontent.com/p/AF1QipMfUkuWb61lVPUrOpCZlL9k8NZKY6NJWQ8Z4rL-=w408-h268-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (2, 4, 31.15307, -87.9838986, 'Gulf Shores Public Beach', 'Tranquil coastline retreat for swimming, boarding, sunbathing & walking.', 'https://lh5.googleusercontent.com/p/AF1QipNMHoBxO3ji6RszR1KV-bQESPs8FZ3naWQIR0km=w408-h306-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, image_url)
+VALUES (2, 4, 31.15307, -87.9838986, 'Dauphin Island East End Public Beach', 'https://lh5.googleusercontent.com/p/AF1QipPY_wWIqF53I8VJ8DVWlin7GLybybQ7U3k1kf_l=w408-h306-k-no');
+
+
+-- CREATED BY FOREST
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 1, 40.582258, -74.154092, 'Dentist Man', 'Making malicious filings', 'https://lh5.googleusercontent.com/p/AF1QipN3Z56WCxEYeNZ3bkaPr4NGx2mXMNrr1VdJt-at=w426-h240-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 1, 51.0370767, -114.0694424, 'Cal-Gary', 'Saw the mysterious Cal-Gary doing some bad-guy stuff', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Naheed_Nenshi_cropped.jpg/1200px-Naheed_Nenshi_cropped.jpg');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 2, 40.744966, -73.9958511, 'Froth', 'Great place for bean juice', 'https://lh5.googleusercontent.com/p/AF1QipNX78qYq-134xeNLVk1AHDPyH8nrHn-w5jJanhe=w408-h287-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 2, 40.7401538, -73.9877831, 'New York City Bagel', 'Love the bagels here!', );
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 4, 30.6028621, -87.9145425, 'Belrose Beach', 'Great sunny spot!', 'https://lh5.googleusercontent.com/p/AF1QipO5wSVRI8V5okHSF99JzxZ-FHT32bgTww7MCcps=w408-h306-k-no');
+
+INSERT INTO pins (creator_id, map_id, latitude, longitute, title, description, image_url)
+VALUES (3, 4, 30.3692936, -88.2471646, 'Rolston Beach', 'Super nice beach', 'https://lh5.googleusercontent.com/p/AF1QipMvdCeRf8VIE0p06SwCViQCuecujiyYzFi-ocF5=w408-h544-k-no');
