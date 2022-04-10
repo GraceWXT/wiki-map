f4227c91db0061a7ff7d4143169bc1eddc16c951
Josh Sparkes <jshsparx@gmail.com>
2022-04-10 17:13:59 -0600

    changed favs file to export db

--- public/scripts/favs.js
+++ public/scripts/favs.js
@@ -1,7 +1,9 @@
+const express = require('express');
+const router = express.Router();
 //require db
 
-$ (document).ready(function() {
-  $(".fav-icon").on("click", function () {
+module.exports = (db) => {
+  router.post('/', (req, res) => {
     //get ids from website/element
     // (userID)
     const mapID = $(".fav-icon").attr("id")
@@ -13,13 +15,17 @@ $ (document).ready(function() {
     AND user_id = $2`, [mapID, userID]).then(res => {
       favsID =res.rows[0].id
       if (!favsID)  {
-        db.query(`INSERT`, [id, id])
+        db.query(`INSERT INTO favs (map_id, user_id)
+        VALUES ($1, $2)`, [id, id])
           .then(CSS).catch()
       } else {
+        db.query(`DELETE`)
         // delete row where id = favs.id
         // .then(CSS)
       }
     }).catch()
   })
-});
+}
+
+
 
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/login/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   // or using plain-text cookies
   res.cookie('user_id', req.params.id);
   // send the user somewhere
