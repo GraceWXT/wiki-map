cb348667e9545ce7de289e273d8ae6765ed4c4bb
Grace Wang <wangxiaotian2012@gmail.com>
2022-04-12 04:51:15 +0000

    Correct some style errors.

--- routes/maps.js
+++ routes/maps.js
@@ -4,15 +4,12 @@ const router = express.Router();
 const { getUserByID } = require('../db/queries');
 
 const getMaps = function(db) {
-
   // express router trims '/maps'
   router.get("/", (req,res) => {
     const id = Number.parseInt(req.cookies["user_id"]);
     console.log("cookie is", typeof id);
-
     getUserByID(db, id)
-      .then((values) => {
-        const user = values[0];
+      .then((user) => {
         console.log("user object:", user);
         res.render("index", { user });
       })
@@ -21,22 +18,7 @@ const getMaps = function(db) {
         console.log("getMaps Error", err.message);
       });
   });
-
-  // return the router
   return router;
 };
 
 module.exports = getMaps;
-
-
-
-// const promise1 = Promise.resolve(3);
-// const promise2 = 42;
-// const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 100, 'foo');
-// });
-
-// Promise.all([promise1, promise2, promise3]).then((values) => {
-//   console.log(values);
-// });
-// // expected output: Array [3, 42, "foo"]
